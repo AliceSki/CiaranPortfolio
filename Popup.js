71d81714b2268f3eee5d8d63e046a9fa5bc303ab
Light <alice.kuikota@gmail.com>
2025-08-02 01:54:15 +0530

    Changes

--- Popup.js
+++ Popup.js
@@ -30,16 +30,43 @@ modal.addEventListener('click', e => {
  function showResult(choice) {
       const responseDiv = document.getElementById('response');
       if (choice === 'Portfolio') {
-        window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=ciaran@cdoherty.co.uk';
-      } else if(choice=='Case') {
-         window.location.href = "https://blog.cdoherty.co.uk/";
+        const BotText = document.getElementById("BotMessage");
+        BotText.innerHTML = '<h2 class="BotGreeting" id="PortfolioButton" onclick="window.location.href=\'https://mail.google.com/mail/?view=cm&fs=1&to=ciaran@cdoherty.co.uk\'">Get My Portfolio</h2>';
+        const UserText = document.getElementById("UserMessage");
+        UserText.innerHTML = '<h3 class="UserGreeting">We\'d like to hire you</h3>'
       }
+      
+      else if(choice=='Case') {
+        const BotText = document.getElementById("BotMessage");
+        BotText.innerHTML = '<h2 class="BotGreeting" id="PortfolioButton" onclick="window.location.href=\'https://mail.google.com/mail/?view=cm&fs=1&to=ciaran@cdoherty.co.uk\'">Some real world cases</h2>';
+        const UserText = document.getElementById("UserMessage");
+        UserText.innerHTML = '<h3 class="UserGreeting">Do you have any real world case studies?</h3>'
+      }
+
       else if(choice=='Hello!'){
-           responseDiv.innerHTML = '<h2>Hello!Have a great day!</h2>';
+        const BotText = document.getElementById("BotMessage");
+          BotText.innerHTML = '<h2 class="BotGreeting">Hello!Have a great day!</h2>';
+          const UserText = document.getElementById("UserMessage");
+          UserText.innerHTML = '<h3 class="UserGreeting">Just saying hello!</h3>';
       }
+
       else if(choice=='Currentprojects'){
-        window.location.href = "https://blog.cdoherty.co.uk/";
+        const BotText = document.getElementById("BotMessage");
+        BotText.innerHTML = '<h2 class="BotGreeting" onclick="window.location.href=\'https://blog.cdoherty.co.uk/\'">Here are some of my projects</h2>';
+      const UserText = document.getElementById("UserMessage");
+      UserText.innerHTML = '<h3 class="UserGreeting">What are you currently working on?</h3>'
       }
+    }
+
+    function hidemenu() {
+      document.getElementById("Menu").style.display = "none";
+    }
+
+    function showmenu(){
+      const container = document.getElementById("MenuBox1");
+        const menu = document.getElementById("Menu");
+        container.appendChild(menu);
+        menu.style.display="block";
     }
 
 const modal1 = document.querySelector('.modal');
@@ -51,3 +78,27 @@ const modal1 = document.querySelector('.modal');
     modal1.classList.remove("scrolled");
   }
 });
+
+function handleclick1(){
+  hidemenu();
+  showResult('Hello!');
+  setTimeout(showmenu, 1000); 
+}
+
+function handleclick2(){
+  hidemenu();
+  showResult('Portfolio');
+  setTimeout(showmenu, 1000); 
+}
+
+function handleclick3(){
+  hidemenu();
+  showResult('Currentprojects');
+  setTimeout(showmenu, 1000); 
+}
+
+function handleclick4(){
+  hidemenu();
+  showResult('Case');
+  setTimeout(showmenu, 1000); 
+}
